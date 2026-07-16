473af66c2f770d7a9fa399d9a6e7cb534cbcdfbe
HarishM-git <harishdeepakh@gmail.com>
2026-07-16 10:10:44 +0530

    day3 progress

--- day3/script.js
+++ day3/script.js
@@ -1,3 +1,10 @@
 function Alerting(){
     alert("you have clicked button")
 }
+
+console.log(test)
+console.log("now only going to declare")
+var test="hello"
+let tt="hello"
+console.log(test)
+console.log(tt)
